20eee4cf3c9370380c5efd582ddfe7ab31b52c24
billy david <williamdavid423@hotmail.com>
2021-04-06 10:14:17 -0400

    Add project files.

---
+++ Pong Reborn but hopefully it works/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Pong_Reborn_but_hopefully_it_works
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.fastButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // fastButton
+            // 
+            this.fastButton.BackColor = System.Drawing.Color.White;
+            this.fastButton.Font = new System.Drawing.Font("NewsGoth BdXCn BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fastButton.Location = new System.Drawing.Point(30, 172);
+            this.fastButton.Name = "fastButton";
+            this.fastButton.Size = new System.Drawing.Size(90, 56);
+            this.fastButton.TabIndex = 0;
+            this.fastButton.Text = "Fast Pace";
+            this.fastButton.UseVisualStyleBackColor = false;
+            this.fastButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.fastButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button fastButton;
+    }
+}
+
---
+++ Pong Reborn but hopefully it works/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong_Reborn_but_hopefully_it_works
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+
+        int paddle1X = 10;
+        int paddle1Y = 170;
+        int player1Score = 0;
+
+        int paddle2X = 580;
+        int paddle2Y = 170;
+        int player2Score = 0;
+
+        int paddleWidth = 10;
+        int paddleHeight = 60;
+        int paddleSpeed = 4;
+
+        int ballX = 295;
+        int ballY = 195;
+        int ballXSpeed = 6;
+        int ballYSpeed = 6;
+        int ballWidth = 10;
+        int ballHeight = 10;
+
+
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (scene == 1)
+            {
+                ballX += ballXSpeed;
+                ballY += ballYSpeed;
+
+                if (wDown == true && paddle1Y > 0)
+                {
+                    paddle1Y -= paddleSpeed;
+                }
+                if (sDown == true && paddle1Y < this.Height - paddleHeight)
+                {
+                    paddle1Y += paddleSpeed;
+                }
+
+                if (upArrowDown == true && paddle2Y > 0)
+                {
+                    paddle2Y -= paddleSpeed;
+                }
+                if (downArrowDown == true && paddle2Y < this.Height - paddleHeight)
+                {
+                    paddle2Y += paddleSpeed;
+                }
+
+                Refresh();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (scene == 1)
+            {
+                e.Graphics.FillRectangle(whiteBrush, ballX, ballY, ballWidth, ballHeight);
+
+                e.Graphics.FillRectangle(blueBrush, paddle1X, paddle1Y, paddleWidth, paddleHeight);
+                e.Graphics.FillRectangle(blueBrush, paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            }
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (scene == 0) { scene = 1; }
+
+            switch (scene)
+            {
+                case 0:
+
+                    break;
+                case 1:
+              
+                    fastButton.Visible = false;
+                    break;
+                case 2:
+
+                    break;
+            }
+        }
+    }
+}
